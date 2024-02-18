360897a1e985d909fa4ee3d5e4e0cec31682461c
Bakhriev <muradbakhriev@gmail.com>
2024-02-18 12:14:42 +0500

    draw sky

---
+++ main.js
@@ -0,0 +1,14 @@
+import { resources } from "./src/resources";
+
+const canvas = document.querySelector(".game-canvas");
+const ctx = canvas.getContext("2d");
+
+const draw = () => {
+	const sky = resources.images.sky;
+	if (sky.isLoaded) {
+		ctx.drawImage(sky.image, 0, 0);
+	}
+	requestAnimationFrame(draw);
+};
+
+requestAnimationFrame(draw);
